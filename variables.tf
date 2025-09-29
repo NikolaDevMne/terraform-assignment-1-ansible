07bf11e331e1958b49968a56f33c8ef6cb89f958
Nikola Vujisic <nikola.vujisic@vegaitglobal.com>
2025-09-29 10:28:16 +0200

    Minor ignore changes, deleted comment from variables and added a remote S3 backend

--- variables.tf
+++ variables.tf
@@ -27,11 +27,3 @@ variable "key_name" {
   type        = string
   default     = null
 }
-
-# variable "db_password" {
-#   description = "RDS admin password"
-#   type        = string
-#   sensitive   = true
-
-#   default = "Nikola2001!"
-# }
--- versions.tf
+++ versions.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  backend "s3" {
+    bucket       = "nikola-vujisic-tfstate-eu-central-1"
+    key          = "env/dev/terraform.tfstate"
+    region       = "eu-central-1"
+    use_lockfile = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"

4dbdf9b32b637559443b872c6eaa3c2d2b8a146f
Nikola Vujisic <nikola.vujisic@vegaitglobal.com>
2025-09-29 12:47:39 +0200

    Hot fix

--- ec2.tf
+++ ec2.tf
@@ -13,7 +13,6 @@ module "ec2_instance" {
   create_eip = true
 
   vpc_security_group_ids = [module.ssh_security_group.security_group_id, module.http_security_group.security_group_id]
-  key_name               = aws_key_pair.ssh.key_name
 
   iam_instance_profile = aws_iam_instance_profile.ec2.name
 
